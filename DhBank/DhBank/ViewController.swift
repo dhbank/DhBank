4d0834f02afa4e3951968fab82738804f24ba06e
Edu Escaleira <duescaleira@gmail.com>
2021-10-20 10:55:26 -0300

    First commit

---
+++ DhBank/DhBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DhBank
+//
+//  Created by Eduardo Escaleira on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DhBank/DhBankUITests/DhBankUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DhBankUITestsLaunchTests.swift
+//  DhBankUITests
+//
+//  Created by Eduardo Escaleira on 20/10/21.
+//
+
+import XCTest
+
+class DhBankUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
