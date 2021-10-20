bbafca17ef733a90c7ed8ca5a0628774a07a53c6
Edu Escaleira <duescaleira@gmail.com>
2021-10-20 11:58:45 -0300

    Layout segunda tela

---
+++ DhBank/DhBank/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  DhBank
+//
+//  Created by Eduardo Escaleira on 20/10/21.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
