617a79df742e35ea917cf85af5318017cc81907c
Edu Escaleira <duescaleira@gmail.com>
2021-10-22 07:37:44 -0300

    teste commit

--- DhBank/DhBank/SecondViewController.swift
+++ DhBank/DhBank/SecondViewController.swift
@@ -16,7 +16,6 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func SeguirButtonPressed(_ sender: Any) {
