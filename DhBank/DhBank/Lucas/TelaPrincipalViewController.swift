8a49dea3f012926fe79678fb7e8885eb93a4c836
Lucas Tamagnone <lucas@lealapps.com>
2021-10-21 23:48:13 -0300

    adiciona botão(converter) na barra superior do teclado

--- DhBank/DhBank/Lucas/TelaPrincipalViewController.swift
+++ DhBank/DhBank/Lucas/TelaPrincipalViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class TelaPrincipalViewController: UIViewController, SelectedRecipeCollectionViewModelCallBack {
         
     // Protocols:
@@ -58,10 +59,37 @@ class TelaPrincipalViewController: UIViewController, SelectedRecipeCollectionVie
         super.viewDidLoad()
                         
         setupCellsCollectionView()
-                
+        self.addDoneButtonOnKeyboard()
+
     }
     
     
+    // adiciona botão(converter) na barra superior do teclado
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
+        doneToolbar.barStyle = UIBarStyle.default
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Converter", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.doneButtonAction))
+
+        let items = NSMutableArray()
+        items.add(flexSpace)
+        items.add(done)
+
+        doneToolbar.items = items as? [UIBarButtonItem]
+        doneToolbar.sizeToFit()
+
+        self.textField.inputAccessoryView = doneToolbar
+        self.textField.inputAccessoryView = doneToolbar
+
+    }
+    // acao do botão(converter)
+    @objc func doneButtonAction()
+    {
+        self.textField.resignFirstResponder()
+    }
+    
+    // esconde o teclado
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         textField.endEditing(true)
     }
@@ -91,4 +119,6 @@ class TelaPrincipalViewController: UIViewController, SelectedRecipeCollectionVie
     }
 
 }
+
+
 
