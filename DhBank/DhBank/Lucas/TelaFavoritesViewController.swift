3d173849617ac5ee4017e4a2041daf90cbea310d
Lucas Tamagnone <lucas@lealapps.com>
2021-10-23 05:46:31 -0300

    update teclado search bar func touchesBegan

--- DhBank/DhBank/Lucas/TelaFavoritesViewController.swift
+++ DhBank/DhBank/Lucas/TelaFavoritesViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class TelaFavoritesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     let dataSource = DataSource()
     
@@ -45,5 +46,10 @@ class TelaFavoritesViewController: UIViewController {
         tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
         tableView.contentInset = UIEdgeInsets(top: 60, left: 0, bottom: 60, right: 0)
         
+    }
+    
+    // esconde o teclado
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        searchBar.endEditing(true)
     }
 }
