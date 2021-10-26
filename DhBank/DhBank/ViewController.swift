1c942a5773a9d429f8bc38994850353a8bed2354
bruno <bruno.p.ornelas@gmail.com>
2021-10-26 14:00:02 -0300

    alteração intro

--- DhBank/DhBank/ViewController.swift
+++ DhBank/DhBank/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoIntroAplication: UIView!
+    @IBOutlet weak var toopView: UIView!
+    @IBOutlet weak var bottonView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        starAnimation()
     }
+    
+    
 
     @IBAction func signInFacebookButton(_ sender: Any) {
         performSegue(withIdentifier: "bemVindoSegue", sender: nil)
@@ -22,6 +28,28 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "bemVindoSegue", sender: nil)
     }
     
+    
+    //Mark: - Animation intro
+    
+    func starAnimation(){
+        toopView.translatesAutoresizingMaskIntoConstraints = true
+        bottonView.translatesAutoresizingMaskIntoConstraints = true
+        UIView.animate(withDuration: 0.7) {
+            self.toopView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.center.y + 15)
+        }
+        
+        
+        UIView.animate(withDuration: 0.5) {
+            self.bottonView.frame = CGRect(x: -150, y: self.view.center.y + 70, width: self.view.frame.size.width + 150, height: self.view.frame.size.height)
+        }
+        
+        UIView.animate(withDuration: 2.5) {
+            self.logoIntroAplication.alpha = 0
+            self.logoIntroAplication.alpha = 1
+            
+        }
+        
+    }
     
 }
 
